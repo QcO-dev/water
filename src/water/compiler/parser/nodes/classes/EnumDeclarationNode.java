81234b3392f7576f2e2f4bbae481034e16f77050
John Parker <johnparker.jjp@gmail.com>
2021-10-17 13:29:15 +0100

    Support for enum's values() /  array

--- src/water/compiler/parser/nodes/classes/EnumDeclarationNode.java
+++ src/water/compiler/parser/nodes/classes/EnumDeclarationNode.java
@@ -6,6 +6,7 @@ import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import water.compiler.FileContext;
 import water.compiler.compiler.Context;
+import water.compiler.compiler.ContextType;
 import water.compiler.compiler.SemanticException;
 import water.compiler.lexer.Token;
 import water.compiler.lexer.TokenType;
@@ -13,6 +14,7 @@ import water.compiler.parser.Node;
 import water.compiler.util.TypeUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnumDeclarationNode implements Node {
 	private final Token name;
@@ -29,9 +31,11 @@ public class EnumDeclarationNode implements Node {
 	public void visit(FileContext fc) throws SemanticException {
 		Context context = fc.getContext();
 
+		ContextType prevType = context.getType();
 		String prevClass = context.getCurrentClass();
 
 		ClassWriter writer = initEnumClass(context);
+		context.setType(ContextType.CLASS);
 
 		Type enumType = Type.getObjectType(context.getCurrentClass());
 
@@ -40,35 +44,20 @@ public class EnumDeclarationNode implements Node {
 
 		context.setMethodVisitor(staticMethod);
 
-		int count = 0;
-		for(Token field : fields) {
-			writer.visitField(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL | Opcodes.ACC_ENUM, field.getValue(), enumType.getDescriptor(), null, null);
+		buildFields(writer, staticMethod, enumType, context);
 
-			staticMethod.visitTypeInsn(Opcodes.NEW, enumType.getInternalName());
-			staticMethod.visitInsn(Opcodes.DUP);
-			staticMethod.visitLdcInsn(field.getValue());
-			TypeUtil.generateCorrectInt(count, context);
-			staticMethod.visitMethodInsn(Opcodes.INVOKESPECIAL, enumType.getInternalName(), "<init>", "(Ljava/lang/String;I)V", false);
-			staticMethod.visitFieldInsn(Opcodes.PUTSTATIC, enumType.getInternalName(), field.getValue(), enumType.getDescriptor());
-		}
+		buildValues(writer, staticMethod, enumType, context);
 
 		staticMethod.visitInsn(Opcodes.RETURN);
 		staticMethod.visitMaxs(0, 0);
 		staticMethod.visitEnd();
 
-		// Build private constructor
-		MethodVisitor constructor = writer.visitMethod(Opcodes.ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
-		constructor.visitCode();
-		constructor.visitVarInsn(Opcodes.ALOAD, 0);
-		constructor.visitVarInsn(Opcodes.ALOAD, 1);
-		constructor.visitVarInsn(Opcodes.ILOAD, 2);
-		constructor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Enum", "<init>", "(Ljava/lang/String;I)V", false);
-		constructor.visitInsn(Opcodes.RETURN);
-		constructor.visitMaxs(0, 0);
-		constructor.visitEnd();
+		buildConstructor(writer, context);
+		buildValuesMethod(writer, enumType, context);
 
 		writer.visitEnd();
 		context.setCurrentClass(prevClass);
+		context.setType(prevType);
 	}
 
 	@Override
@@ -81,28 +70,41 @@ public class EnumDeclarationNode implements Node {
 		Type enumType = Type.getObjectType(context.getCurrentClass());
 
 		// Build static method outline
-		MethodVisitor staticMethod = writer.visitMethod(Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
-		staticMethod.visitCode();
-		staticMethod.visitInsn(Opcodes.RETURN);
-		staticMethod.visitMaxs(0, 0);
-		staticMethod.visitEnd();
+		{
+			MethodVisitor staticMethod = writer.visitMethod(Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
+			staticMethod.visitCode();
+			staticMethod.visitInsn(Opcodes.RETURN);
+			staticMethod.visitMaxs(0, 0);
+			staticMethod.visitEnd();
+		}
 
 		// Build private constructor
-		MethodVisitor constructor = writer.visitMethod(Opcodes.ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
-		constructor.visitCode();
-		// For bytecode checker
-		constructor.visitVarInsn(Opcodes.ALOAD, 0);
-		constructor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Enum", "<init>", "()V", false);
-		constructor.visitInsn(Opcodes.RETURN);
-		constructor.visitMaxs(0, 0);
-		constructor.visitEnd();
+		{
+			MethodVisitor constructor = writer.visitMethod(Opcodes.ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
+			constructor.visitCode();
+			// For bytecode checker
+			constructor.visitVarInsn(Opcodes.ALOAD, 0);
+			constructor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Enum", "<init>", "()V", false);
+			constructor.visitInsn(Opcodes.RETURN);
+			constructor.visitMaxs(0, 0);
+			constructor.visitEnd();
+		}
+
+		{
+			MethodVisitor values = writer.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "values", "()[" + enumType.getDescriptor(), null, null);
+			values.visitCode();
+			values.visitInsn(Opcodes.ACONST_NULL);
+			values.visitInsn(Opcodes.ARETURN);
+			values.visitMaxs(0, 0);
+			values.visitEnd();
+		}
 
 		// Fields
 
 		for(Token field : fields) {
 			writer.visitField(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL | Opcodes.ACC_ENUM, field.getValue(), enumType.getDescriptor(), null, null);
 		}
-
+		writer.visitField(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL | Opcodes.ACC_SYNTHETIC, "$VALUES", "[" + enumType.getDescriptor(), null, null);
 
 		writer.visitEnd();
 		context.setCurrentClass(prevClass);
@@ -154,8 +156,73 @@ public class EnumDeclarationNode implements Node {
 		return writer;
 	}
 
+	private void buildFields(ClassWriter writer, MethodVisitor staticMethod, Type enumType, Context context) {
+		int count = 0;
+		for(Token field : fields) {
+			writer.visitField(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL | Opcodes.ACC_ENUM, field.getValue(), enumType.getDescriptor(), null, null);
+
+			context.updateLine(field.getLine());
+			staticMethod.visitTypeInsn(Opcodes.NEW, enumType.getInternalName());
+			staticMethod.visitInsn(Opcodes.DUP);
+			staticMethod.visitLdcInsn(field.getValue());
+			TypeUtil.generateCorrectInt(count, context);
+			staticMethod.visitMethodInsn(Opcodes.INVOKESPECIAL, enumType.getInternalName(), "<init>", "(Ljava/lang/String;I)V", false);
+			staticMethod.visitFieldInsn(Opcodes.PUTSTATIC, enumType.getInternalName(), field.getValue(), enumType.getDescriptor());
+		}
+	}
+
+	private void buildValues(ClassWriter writer, MethodVisitor staticMethod, Type enumType, Context context) {
+		writer.visitField(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL | Opcodes.ACC_SYNTHETIC, "$VALUES", "[" + enumType.getDescriptor(), null, null);
+
+		TypeUtil.generateCorrectInt(fields.size(), context);
+		staticMethod.visitTypeInsn(Opcodes.ANEWARRAY, enumType.getInternalName());
+		for(int i = 0; i < fields.size(); i++) {
+			Token field = fields.get(i);
+
+			staticMethod.visitInsn(Opcodes.DUP);
+			TypeUtil.generateCorrectInt(i, context);
+			staticMethod.visitFieldInsn(Opcodes.GETSTATIC, enumType.getInternalName(), field.getValue(), enumType.getDescriptor());
+			staticMethod.visitInsn(Opcodes.AASTORE);
+		}
+		staticMethod.visitFieldInsn(Opcodes.PUTSTATIC, enumType.getInternalName(), "$VALUES", "[" + enumType.getDescriptor());
+	}
+
+	private void buildConstructor(ClassWriter writer, Context context) {
+		MethodVisitor constructor = writer.visitMethod(Opcodes.ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
+		constructor.visitCode();
+		context.setMethodVisitor(constructor);
+		context.updateLine(name.getLine());
+		constructor.visitVarInsn(Opcodes.ALOAD, 0);
+		constructor.visitVarInsn(Opcodes.ALOAD, 1);
+		constructor.visitVarInsn(Opcodes.ILOAD, 2);
+		constructor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Enum", "<init>", "(Ljava/lang/String;I)V", false);
+		constructor.visitInsn(Opcodes.RETURN);
+		constructor.visitMaxs(0, 0);
+		constructor.visitEnd();
+	}
+
+	private void buildValuesMethod(ClassWriter writer, Type enumType, Context context) {
+		MethodVisitor values = writer.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "values", "()[" + enumType.getDescriptor(), null, null);
+		values.visitCode();
+		context.setMethodVisitor(values);
+		context.updateLine(name.getLine());
+
+		values.visitFieldInsn(Opcodes.GETSTATIC, enumType.getInternalName(), "$VALUES", "[" + enumType.getDescriptor());
+		values.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "[" + enumType.getDescriptor(), "clone", "()Ljava/lang/Object;", false);
+		values.visitTypeInsn(Opcodes.CHECKCAST, "[" + enumType.getDescriptor());
+
+		values.visitInsn(Opcodes.ARETURN);
+		values.visitMaxs(0, 0);
+		values.visitEnd();
+	}
+
 	private int getAccessLevel() {
 		if(access == null || access.getType() == TokenType.PUBLIC) return Opcodes.ACC_PUBLIC;
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "enum %s {%s\n}".formatted(name.getValue(), fields.stream().map(Token::getValue).collect(Collectors.joining(", ")));
 	}
 }
