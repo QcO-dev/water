bb76f15a3c592320f35095ea3251f267a516061b
John Parker <johnparker.jjp@gmail.com>
2021-11-01 13:36:18 +0000

    Added Runtime Module and Nullable annotation

---
+++ Runtime/src/water/runtime/annotation/Nullable.java
@@ -0,0 +1,12 @@
+package water.runtime.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface Nullable {
+
+}
